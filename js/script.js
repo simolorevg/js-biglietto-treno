f1baaa41873cf90cf3619c96eeac782f80f2b388
simolorevg <simoneviaggi@libero.it>
2023-03-08 15:30:21 +0100

    main script done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 let userPathLenght = prompt('Quanti chilometri vuole percorrere?');
-console.log(userPathLenght, typeof userPathLenght);
+let finalPrice;
+let message;
 if(!isNaN(userPathLenght)){ // se è stato inserito un numero allora prosegue con il programma
     userPathLenght = parseInt(userPathLenght);
-    console.log(userPathLenght, typeof userPathLenght);
     let userAge = prompt('Inserire la propria età');
-    console.log(userAge, typeof userAge);
     userAge = parseInt(userAge);
-    console.log(userAge, typeof userAge);
+    finalPrice = userPathLenght * 0.21;
+    console.log(finalPrice);
+    if(userAge < 18){
+        finalPrice = finalPrice * 0.2;
+        message = 'Prezzo scontato per minorenni: ';
+    } else if(userAge > 65){
+        finalPrice= finalPrice * 0.4;
+        message = 'Prezzo scontato per Over 65: '
+    }
 }else{ // altrimenti blocca tutto
     console.log('Errore! Dato non inserito correttamente: ricarica la pagina.')
 }
+finalPrice = finalPrice.toFixed(2);
+message = message + finalPrice + '€';
+document.getElementById('result').innerHTML = message;
