8982915aaac81b8fe23020320aebc467b6e2df2f
simolorevg <simoneviaggi@libero.it>
2023-03-08 16:04:58 +0100

    Put some comment to make readble

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@ let userPathLenght = prompt('Quanti chilometri vuole percorrere?');
 let userAge = prompt('Inserire la propria età');
 let finalPrice;
 let message = "Prezzo: ";
-if((!isNaN(userPathLenght)) && (!isNaN(userAge))){ // se è stato inserito un numero allora prosegue con il programma
-    userPathLenght = parseInt(userPathLenght);
-    userAge = parseInt(userAge);
-    finalPrice = userPathLenght * 0.21;
+if((!isNaN(userPathLenght)) && (!isNaN(userAge))){ //se è stato inserito un numero allora prosegue con il programma
+    userPathLenght = parseInt(userPathLenght); //lo converto in numero
+    userAge = parseInt(userAge);//lo converto in numero
+    finalPrice = userPathLenght * 0.21;//calcolo il prezzo senza sconti
     console.log(finalPrice);
-    if(userAge < 18){
+    if(userAge < 18){// se è minorenne allora 20% sconto
         finalPrice = finalPrice * 0.2;
         message = 'Prezzo scontato per minorenni: ';
-    } else if(userAge > 65){
+    } else if(userAge > 65){// altrimenti se è over 65 sconto 40%
         finalPrice= finalPrice * 0.4;
         message = 'Prezzo scontato per Over 65: '
     }
-}else{ // altrimenti blocca tutto
+}else{ //altrimenti sono stati messi dato non corretti e blocca tutto
     message = "ERRORE: DATI NON INSERITI CORRETTAMENTE. RICARICA LA PAGINA!!";
     document.getElementById('result').innerHTML = message;
 }
-finalPrice = finalPrice.toFixed(2);
-message = message + finalPrice + '€';
-document.getElementById('result').innerHTML = message;
+finalPrice = finalPrice.toFixed(2);//riduco a 2 decimali il prezzo finale
+message = message + finalPrice + '€';// riscrivo il messaggio finale
+document.getElementById('result').innerHTML = message;//lo stampo sulla pagina html
