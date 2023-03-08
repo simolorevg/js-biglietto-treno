93840c71f04db5196fe8690c18062f8528becec5
simolorevg <simoneviaggi@libero.it>
2023-03-08 15:14:23 +0100

    starting to write the script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert('Hello world');
+let userPathLenght = prompt('Quanti chilometri vuole percorrere?');
+console.log(userPathLenght, typeof userPathLenght);
+if(!isNaN(userPathLenght)){ // se è stato inserito un numero allora prosegue con il programma
+    userPathLenght = parseInt(userPathLenght);
+    console.log(userPathLenght, typeof userPathLenght);
+    let userAge = prompt('Inserire la propria età');
+    console.log(userAge, typeof userAge);
+    userAge = parseInt(userAge);
+    console.log(userAge, typeof userAge);
+}else{ // altrimenti blocca tutto
+    console.log('Errore! Dato non inserito correttamente: ricarica la pagina.')
+}
