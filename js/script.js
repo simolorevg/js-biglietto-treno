37e7401bc278446277df1077c12d909c06bf97fe
simolorevg <simoneviaggi@libero.it>
2023-03-09 09:57:06 +0100

    fixed the math problem

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ if((!isNaN(userPathLenght)) && (!isNaN(userAge))){ //se è stato inserito un num
     finalPrice = userPathLenght * 0.21;//calcolo il prezzo senza sconti
     console.log(finalPrice);
     if(userAge < 18){// se è minorenne allora 20% sconto
-        finalPrice = finalPrice * 0.2;
+        finalPrice = finalPrice - (finalPrice * 0.2);
         message = 'Prezzo scontato per minorenni: ';
     } else if(userAge > 65){// altrimenti se è over 65 sconto 40%
-        finalPrice= finalPrice * 0.4;
+        finalPrice= finalPrice - (finalPrice * 0.4);
         message = 'Prezzo scontato per Over 65: '
     }
     finalPrice = finalPrice.toFixed(2);//riduco a 2 decimali il prezzo finale
